fe33e0c3c578e91e67db8a42d46d975ab2ac6a91
MattScuteri <mttscuteri@gmail.com>
2019-02-04 21:03:50 -0500

    index page set, styling, button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Movies To Watch To-Do List';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
