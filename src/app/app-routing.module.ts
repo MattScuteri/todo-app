5287219713cdfac156f52b47e5cc175f2369337a
MattScuteri <mttscuteri@gmail.com>
2019-02-04 22:06:16 -0500

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { AppComponent } from './app.component';
+
+const toDoRoutes: Routes = [
+  {path: "todo", component: ToDoListComponent },
+  {path: "home", component: AppComponent}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(toDoRoutes)
   ]
 })
 export class AppRoutingModule { }
