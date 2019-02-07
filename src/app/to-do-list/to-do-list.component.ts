5a9981967735969ab8e4c49e1cff861645acd6c2
MattScuteri <mttscuteri@gmail.com>
2019-02-06 19:30:22 -0500

    arrays edits, ngfor looping, click function

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
-  constructor() { }
+  moviesToWatch = [];
+  moviesAlreadyWatched = [];
 
-  ngOnInit() {
+  addMovie(newMovie) {
+    this.moviesToWatch.push(newMovie);
   }
+
+  ngOnInit() {}
 
 }
